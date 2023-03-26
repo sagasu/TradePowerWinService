77feafe1ffc026fa4bdea15d4c5a28798b7c4bb5
Mateusz Kopij <sagasu@gmail.com>
2023-03-26 14:46:53 +0100

    Making sure that we are testing the right thing in TradeProcessorServiceTest

--- TradePowerWinService.Tests/Services/TradeProcessorServiceTest.cs
+++ TradePowerWinService.Tests/Services/TradeProcessorServiceTest.cs
@@ -12,22 +12,71 @@ namespace TradePowerWinService.Tests.Services
     [TestClass]
     public class TradeProcessorServiceTest
     {
+        private const int ANY_NUMBER = 10;
+
         [TestMethod]
-        public void GetAggregatedPowerDtos_MultiplePeriods_VolumeAggregatedCorrectly()
+        public void GetAggregatedPowerDtos_TwoTradesTwoPeriods_FirstPeriodVolumeAggregatedCorrectly()
+        {
+            var firstTradeFirstPeriodVolume = 3;
+            var secondTradeFirstPeriodVolume = 4;
+            var powerTradeDtos = GetPowerTradeDtos(firstTradeFirstPeriodVolume: firstTradeFirstPeriodVolume, secondTradeFirstPeriodVolume: secondTradeFirstPeriodVolume);
+            var tradeProcessorService = GetTradeProcessorService();
+            
+            var aggregatedPowerDtos = tradeProcessorService.GetAggregatedPowerDtos(powerTradeDtos);
+
+            Assert.AreEqual(firstTradeFirstPeriodVolume + secondTradeFirstPeriodVolume, aggregatedPowerDtos[0].Volume);
+        }
+        
+        [TestMethod]
+        public void GetAggregatedPowerDtos_TwoTradesTwoPeriods_SecondPeriodVolumeAggregatedCorrectly()
+        {
+            var firstTradeSecondPeriodVolume = 5;
+            var secondTradeSecondPeriodVolume = 6;
+            var powerTradeDtos = GetPowerTradeDtos(firstTradeSecondPeriodVolume: firstTradeSecondPeriodVolume, secondTradeSecondPeriodVolume: secondTradeSecondPeriodVolume);
+            var tradeProcessorService = GetTradeProcessorService();
+            
+
+            var aggregatedPowerDtos = tradeProcessorService.GetAggregatedPowerDtos(powerTradeDtos);
+
+            Assert.AreEqual(firstTradeSecondPeriodVolume + secondTradeSecondPeriodVolume, aggregatedPowerDtos[1].Volume);
+        }
+
+        private static List<PowerTradeDto> GetPowerTradeDtos(int firstTradeFirstPeriodVolume = ANY_NUMBER, int firstTradeSecondPeriodVolume = ANY_NUMBER, int secondTradeFirstPeriodVolume = ANY_NUMBER, int secondTradeSecondPeriodVolume = ANY_NUMBER)
+        {
+            var anyDate = DateTime.Now;
+            var powerTradeDtos = new List<PowerTradeDto>
+            {
+                new PowerTradeDto
+                {
+                    Date = anyDate,
+                    Periods = new List<PowerPeriodDto>
+                    {
+                        new PowerPeriodDto { Period = 1, Volume = firstTradeFirstPeriodVolume },
+                        new PowerPeriodDto { Period = 2, Volume = firstTradeSecondPeriodVolume }
+                    }
+                },
+                new PowerTradeDto
+                {
+                    Date = anyDate,
+                    Periods = new List<PowerPeriodDto>
+                    {
+                        new PowerPeriodDto { Period = 1, Volume = secondTradeFirstPeriodVolume },
+                        new PowerPeriodDto { Period = 2, Volume = secondTradeSecondPeriodVolume }
+                    }
+                },
+            };
+            return powerTradeDtos;
+        }
+
+        private static TradeProcessorService GetTradeProcessorService()
         {
             var tradeDataService = new Mock<ITradeDataService>();
             var exportService = new Mock<IExportService>();
             var dateTimeService = new Mock<IDateTimeService>();
             dateTimeService.Setup(x => x.Parse(It.IsAny<string>())).Returns(DateTime.Parse(TradeProcessorService.START_TIME));
-            var powerTradeDtos = new List<PowerTradeDto>
-            {
-                new PowerTradeDto{Date = DateTime.Now, Periods = new List<PowerPeriodDto>{ new PowerPeriodDto{Period = 0, Volume = 1}, new PowerPeriodDto{Period = 1, Volume = 2}}},
-                new PowerTradeDto{Date = DateTime.Now, Periods = new List<PowerPeriodDto>{ new PowerPeriodDto{Period = 0, Volume = 22}, new PowerPeriodDto{Period = 1, Volume = 22}}},
-            }; 
-
-            var tradeProcessorService = new TradeProcessorService(tradeDataService.Object, exportService.Object, dateTimeService.Object);
-            var aggregatedPowerDtos = tradeProcessorService.GetAggregatedPowerDtos(powerTradeDtos);
-            Assert.AreEqual(3, aggregatedPowerDtos.Count);
+            var tradeProcessorService =
+                new TradeProcessorService(tradeDataService.Object, exportService.Object, dateTimeService.Object);
+            return tradeProcessorService;
         }
     }
 }
